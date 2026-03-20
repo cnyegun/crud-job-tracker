d51d515979691d4ba5d2ee8a9ef33ec6e6428bf3
Chi Nguyen <buichinguyen1105@gmail.com>
2026-03-20 23:32:24 +0200

    enable cors

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
+const cors = require('cors')
 const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
 const express = require('express')
 const app = express()
 app.use(express.json())
+app.use(cors())
 
 const SECRET_KEY = process.env.SECRET_KEY
 
